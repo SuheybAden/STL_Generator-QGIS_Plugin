0671ca5563b095597bbd636a86490561544a57e8
Suheyb M Aden <suheyb1@gmail.com>
2022-07-25 11:33:29 -0700

    Removed open3d dependency

--- mesh_generator.py
+++ mesh_generator.py
@@ -6,9 +6,7 @@ import math
 # )
 
 import numpy as np
-import open3d as o3d
 from osgeo import gdal, ogr
-import pip._internal as pip
 
 
 class EdgePoint(Enum):
@@ -40,17 +38,8 @@ class MeshGenerator:
         self.lineWidth = lineWidth
 
     def dem_to_mesh(self, source_dem):
-        # try:
-        #     pip.main(['install', 'open3d'])
-        #     import open3d as o3d
-        # except ImportError:
-        #     print("Couldn't install open3d. Either restart the plugin or install open3d manually through the python console.")
-        #     return
         input_array = self.generate_height_array(source_dem=source_dem)
         self.manually_generate_stl(input_array)
-
-        # pcd = self.generate_point_cloud(array=input_array)
-        # self.pcdToMesh(pcd=pcd)
 
     def generate_height_array(self, source_dem):
         dem = gdal.Open(source_dem, gdal.GA_ReadOnly)
@@ -99,7 +88,7 @@ class MeshGenerator:
         self.bottomLevel = (
             minValue * self.verticalExaggeration) - self.baseHeight
 
-        np.save("C:/Code/array_data.npy", array)
+        # np.save("C:/Code/array_data.npy", array)
 
         # # Debugging logs for checking scaling values
         # QgsMessageLog.logMessage(
@@ -182,101 +171,6 @@ class MeshGenerator:
             self.write_face([[x1, y1, self.bottomLevel],
                              [x2, y2, self.bottomLevel],
                              [x2, y2, array[x2][y2]*self.verticalExaggeration]])
-
-    def generate_point_cloud(self, array):
-        # ***************************** GENERATE POINTCLOUD FROM IMAGE ARRAY ************************ #
-        v = []
-        n = []
-        normal = 1
-        x_size = array.shape[0]
-        y_size = array.shape[1]
-
-        # Debugging logs for checking mismatch in expected nodata value
-        # QgsMessageLog.logMessage(
-        #     str(noDataValue), level=Qgis.Info)
-        # QgsMessageLog.logMessage(
-        #     str(array[0][0]), level=Qgis.Info)
-
-        for x in range(x_size):
-            for y in range(y_size):
-                if array[x][y] != self.noDataValue:
-                    # Add the top facing points
-                    # These are the ones that show the actual topography
-                    point_height = array[x][y] * self.verticalExaggeration
-                    v.append([x, y, point_height])
-                    n.append([0, 0, normal])
-
-                    # If this point is an edge, add points going down the side
-                    if (self.isEdgePoint(x, y, array)):
-                        self.addSidePoints(
-                            x, y, v, n, edge_height=point_height)
-
-                    # Add points to make a flat bottom
-                    v.append([x, y, self.bottomLevel])
-                    n.append([0, 0, -normal])
-
-        vertices = np.array(v)
-        normals = np.array(n)
-        v = []
-
-        pcd = o3d.geometry.PointCloud()
-        pcd.points = o3d.utility.Vector3dVector(vertices)
-        pcd.normals = o3d.utility.Vector3dVector(normals)
-
-        # pcd.normals = o3d.utility.Vector3dVector(np.zeros((1, 3)))
-        # pcd.estimate_normals(fast_normal_computation=True)
-        # pcd.orient_normals_consistent_tangent_plane(3)
-
-        # STUFF FOR TRYING TO REMOVE OUTLIERS
-        # voxel_down_pcd = pcd.voxel_down_sample(voxel_size=0.02)
-        # cl, ind = voxel_down_pcd.remove_radius_outlier(nb_points=15, radius=3)
-
-        # inlier_cloud = voxel_down_pcd.select_by_index(ind)
-        # inlier_cloud.paint_uniform_color([0.8, 0.8, 0.8])
-
-        # outlier_cloud = voxel_down_pcd.select_by_index(ind, invert=True)
-        # outlier_cloud.paint_uniform_color([1.0, 0.0, 0.0])
-
-        # print("Successfully generated point cloud")
-        # QgsMessageLog.logMessage(
-        #     "Successfully generated point cloud", level=Qgis.Info)
-
-        # Display pointcloud
-        o3d.visualization.draw_geometries([pcd])
-
-        return pcd
-
-    def pcdToMesh(self, pcd):
-        # # Two ways of getting radius for ball pivoting reconstruction
-        # distances = pcd.compute_nearest_neighbor_distance()
-        # avg_dist = np.mean(distances)
-        # radius = 4 * avg_dist
-
-        # gt = dem.GetGeoTransform()
-        # radius = 5.8 * max(gt[1], gt[5])
-
-        # Ball Pivoting Algorithm
-        # Need normals
-        bpa_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
-            pcd, o3d.utility.DoubleVector([.15, .5, 1, 1.5, 4, 10]))
-
-        dec_bpa = bpa_mesh.simplify_quadric_decimation(100000)
-        dec_bpa.remove_degenerate_triangles()
-        dec_bpa.remove_duplicated_triangles()
-        dec_bpa.remove_duplicated_vertices()
-        dec_bpa.remove_non_manifold_edges()
-
-        # Poisson Algorithm
-        # poisson_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-        #     pcd, depth=12, scale=2)[0]
-
-        # bbox = pcd.get_axis_aligned_bounding_box()
-        # p_mesh_crop = poisson_mesh.crop(bbox)
-
-        # dec_poisson = poisson_mesh.simplify_quadric_decimation(100000)
-
-        o3d.visualization.draw_geometries([dec_bpa])
-        o3d.io.write_triangle_mesh(self.saveLocation + ".stl", dec_bpa)
 
     # Returns whether or not a data point is on the edge of the DEM or not
 
