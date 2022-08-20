e86f0e391c77ac6bad1d4a7f7a4783185bd02252
Suheyb M Aden <suheyb1@gmail.com>
2022-08-20 15:01:04 -0700

    Removed unnecessary Python code

--- mesh_generator.py
+++ mesh_generator.py
@@ -122,7 +122,7 @@ class MeshGenerator:
         start_time = time.time()
 
         np_float_pointer = np.ctypeslib.ndpointer(
-            dtype=np.float32, ndim=2, flags="C")
+            dtype=np.float32, ndim=2, flags="C_CONTIGUOUS")
 
         self.lib.generateSTL.argtypes = [np_float_pointer, ctypes.c_int, ctypes.c_int,
                                          ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_char_p]
@@ -131,97 +131,5 @@ class MeshGenerator:
         self.lib.generateSTL(array.astype(np.float32), array.shape[0], array.shape[1], self.noDataValue,
                              self.lineWidth, self.bottomLevel, bytes(self.saveLocation, 'utf-8'))
 
-        # self.numTriangles = 0
-        # header = [0] * 80
-
-        # with open(self.saveLocation, "wb+") as file:
-        #     file.write(struct.pack('<' + 'B' * len(header), * header))
-        #     file.write(struct.pack('<I', 0))
-
-        # size_x, size_y = array.shape
-        # for x in range(size_x - 1):
-        #     for y in range(size_y - 1):
-        #         if(not self.abort):
-        #             # Get a 2x2 window
-        #             window = array[x:x+2, y:y+2]
-
-        #             orientation = np.packbits(
-        #                 (window != self.noDataValue).flatten())
-
-        #             if (window[0][0] != self.noDataValue and
-        #                     window[1][0] != self.noDataValue and
-        #                     window[0][1] != self.noDataValue):
-
-        #                 # Add top face
-        #                 self.write_face([[x, y, window[0][0]],
-        #                                  [x, y + 1, window[0][1]],
-        #                                  [x + 1, y, window[1][0]]])
-        #                 # Add bottom face
-        #                 self.write_face([[x, y, self.bottomLevel],
-        #                                  [x + 1, y, self.bottomLevel],
-        #                                  [x, y + 1, self.bottomLevel]])
-        #                 # Add side faces
-        #                 self.add_side_face(x, y, x + 1, y, array)
-        #                 self.add_side_face(x, y, x, y + 1, array)
-        #                 self.add_side_face(x + 1, y, x, y + 1, array)
-
-        #             if (window[1][1] != self.noDataValue and
-        #                     window[1][0] != self.noDataValue and
-        #                     window[0][1] != self.noDataValue):
-        #                 # Add top face
-        #                 self.write_face([[x + 1, y + 1, window[1][1]],
-        #                                  [x + 1, y, window[1][0]],
-        #                                  [x, y + 1, window[0][1]]])
-        #                 # Add bottom face
-        #                 self.write_face([[x + 1, y + 1, self.bottomLevel],
-        #                                  [x, y + 1, self.bottomLevel],
-        #                                  [x + 1, y, self.bottomLevel]])
-        #                 # Add side faces
-        #                 self.add_side_face(x + 1, y + 1, x + 1, y, array)
-        #                 self.add_side_face(x + 1, y + 1, x, y + 1, array)
-        #                 self.add_side_face(x + 1, y, x, y + 1, array)
-        #         else:
-        #             self.abort = False
-        #             return
-
-        # print(self.numTriangles)
-
-        # # Write the header and number of triangles to the beginning of the file
-        # with open(self.saveLocation, "r+b") as file:
-        #     file.seek(0)
-        #     file.write(struct.pack('<' + 'B'*len(header), *header))
-        #     file.write(struct.pack('<I', self.numTriangles))
-
-        print("Time to generate STL: " + str(time.time() - start_time))
-
         QgsMessageLog.logMessage(
             "Time to generate STL: " + str(time.time() - start_time), level=Qgis.Info)
-
-    def write_face(self, vertices):
-        self.numTriangles += 1
-
-        with open(self.saveLocation, "ab") as file:
-            # Writes normal vector
-            normal = [0.0, 0.0, 0.0]
-            file.write(struct.pack('<' + 'f'*len(normal), *normal))
-
-            # Writes the 3 vertices
-            for row in vertices:
-                file.write(struct.pack('<' + 'f'*len(row), *row))
-
-            # Writes attribute byte count
-            file.write(struct.pack('<H', 0))
-
-    def add_side_face(self, x1, y1, x2, y2, array):
-        if(self.isEdgePoint(x1, y1, array) and self.isEdgePoint(x2, y2, array)):
-            self.write_face([[x1, y1, array[x1][y1]],
-                             [x1, y1, self.bottomLevel],
-                             [x2, y2, array[x2][y2]]])
-            self.write_face([[x1, y1, self.bottomLevel],
-                             [x2, y2, self.bottomLevel],
-                             [x2, y2, array[x2][y2]]])
-
-    # Returns whether or not a data point will be on the edge of the model
-    def isEdgePoint(self, x, y, array):
-        window = array[x-1:x+2, y-1:y+2]
-        return window.shape != (3, 3) or (self.noDataValue in window)
