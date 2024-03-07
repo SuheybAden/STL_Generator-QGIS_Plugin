d7410d2e3b0ca32ee0cbf04d6686f76822cd0a01
Suheyb M Aden <suheyb1@gmail.com>
2024-03-07 02:04:11 -0800

    Fixed typo with ofstream

--- backend/MeshGenerator/meshgenerator.h
+++ backend/MeshGenerator/meshgenerator.h
@@ -9,8 +9,8 @@ struct Parameters;
 extern "C" MESHGENERATOR_EXPORT void generateSTL(float* v, int width, int height,
                                                  float noDataValue, float lineWidth, float bottomLevel, const char* filename);
 
-void writeFace(float vertices[3][3], Parameters& p, std::ostream& fp);
-void addSideFace(float x1, float x2, float y1, float y2, Parameters& p, std::ostream& fp);
+void writeFace(float vertices[3][3], Parameters& p, std::ofstream& file);
+void addSideFace(float x1, float x2, float y1, float y2, Parameters& p, std::ofstream& file);
 bool isEdgePoint(int x, int y, Parameters p);
 float getArrayValue(int xIndex, int yIndex, Parameters p);
 
