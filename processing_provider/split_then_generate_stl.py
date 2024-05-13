226585fe9608596dc4c6412471fcf49e070187ed
Suheyb Aden <suheyb1@gmail.com>
2024-05-13 02:06:08 -0700

    cleaned up the print outs and comments

--- processing_provider/split_then_generate_stl.py
+++ processing_provider/split_then_generate_stl.py
@@ -152,7 +152,7 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 self.tr("Model Height (mm)"),
                 type=QgsProcessingParameterNumber.Double,
                 defaultValue=10.0,
-                minValue=0
+                minValue=0,
             )
         )
 
@@ -163,7 +163,7 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 self.tr("Base Thickness (mm)"),
                 type=QgsProcessingParameterNumber.Double,
                 defaultValue=10.0,
-                minValue=0
+                minValue=0,
             )
         )
 
@@ -174,7 +174,7 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 self.tr("Total Width (mm)"),
                 type=QgsProcessingParameterNumber.Double,
                 defaultValue=200.0,
-                minValue=0.0
+                minValue=0.0,
             )
         )
 
@@ -185,7 +185,7 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 self.tr("Total Length (mm)"),
                 type=QgsProcessingParameterNumber.Double,
                 defaultValue=200.0,
-                minValue=0.0
+                minValue=0.0,
             )
         )
 
@@ -196,7 +196,7 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 self.tr("Bed Width (mm)"),
                 type=QgsProcessingParameterNumber.Double,
                 defaultValue=200.0,
-                minValue=0
+                minValue=0,
             )
         )
 
@@ -207,7 +207,7 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 self.tr("Bed Length (mm)"),
                 type=QgsProcessingParameterNumber.Double,
                 defaultValue=200.0,
-                minValue=0
+                minValue=0,
             )
         )
 
@@ -218,7 +218,7 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 self.tr("Line Width (mm)"),
                 type=QgsProcessingParameterNumber.Double,
                 defaultValue=0.4,
-                minValue=0
+                minValue=0,
             )
         )
 
@@ -234,7 +234,8 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
         Here is where the processing itself takes place.
         """
 
-        # Load all the parameters
+        # **************************************************************************************************
+        # 1) LOAD ALL THE PARAMETERS
         orig_raster_layer = self.parameterAsRasterLayer(
             parameters, self.INPUT_RASTER, context
         )
@@ -252,15 +253,16 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
         )
         bed_width = self.parameterAsDouble(parameters, self.BED_WIDTH, context)
         bed_length = self.parameterAsDouble(parameters, self.BED_LENGTH, context)
-        total_width = self.parameterAsDouble(
-            parameters, self.TOTAL_WIDTH, context)
-        total_length = self.parameterAsDouble(
-            parameters, self.TOTAL_LENGTH, context)
+        total_width = self.parameterAsDouble(parameters, self.TOTAL_WIDTH, context)
+        total_length = self.parameterAsDouble(parameters, self.TOTAL_LENGTH, context)
         line_width = self.parameterAsDouble(parameters, self.LINE_WIDTH, context)
         dest_folder = self.parameterAsFile(parameters, self.OUTPUT, context)
 
         # Send some information to the user
         feedback.pushInfo("Loaded all the parameters\n")
+
+        # **************************************************************************************************
+        # 2) REPROJECT THE VECTOR LAYER IF NECESSARY
 
         # Change the projection of the vector layer to match the raster layer if it doesn't already
         if orig_vector_layer.crs() != orig_raster_layer.crs():
@@ -269,12 +271,17 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 {
                     "INPUT": vector_path,
                     "TARGET_CRS": orig_raster_layer.crs(),
-                    "OUTPUT": "TEMPORARY_OUTPUT"
-                }
+                    "OUTPUT": "TEMPORARY_OUTPUT",
+                },
             )["OUTPUT"]
 
             # Send some information to the user
-            feedback.pushInfo("Reprojected the vector layer\n")
+            feedback.pushInfo(
+                f"Reprojected the vector layer to {orig_vector_layer.crs()}"
+            )
+
+        # **************************************************************************************************
+        # 3) SPLIT THE VECTOR LAYER ACCORDING TO THE FIELD CHOSEN BY THE USER
 
         # Split the multi polygon layer into multiple vector layers using the "split vector layer" algorithm
         vector_filenames: list[str] = processing.run(
@@ -284,42 +291,40 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                 "FILE_TYPE": 0,
                 "INPUT": vector_path,
                 "OUTPUT": "TEMPORARY_OUTPUT",
-                "PREFIX_FIELD": True
-            }
+                "PREFIX_FIELD": True,
+            },
         )["OUTPUT_LAYERS"]
 
         # Send some information to the user
-        feedback.pushInfo("Split the vector file")
+        feedback.pushInfo(f"Split the vector file based on the {field} field")
 
-        # Clip the raster file using the vector masks and
-        # find the scale factor required to fit the largest STL onto the print bed
+        # **************************************************************************************************
+        # 4) CLIP THE RASTER LAYER AND FIND THE APPROPRIATE SCALE FACTOR
+
         bed_scale_factor = 1.0
         rasters_to_process: list[QgsRasterLayer] = []
+
+        # Clip the raster file using the vector masks and
+        # find the scale factor required to fit the largest clipped raster onto the print bed
         for mask_filename in vector_filenames:
-            layer = QgsVectorLayer(path=mask_filename)
-            overlap = layer.extent().intersect(orig_raster_layer.extent())
+            mask_layer = QgsVectorLayer(path=mask_filename)
+            overlap = mask_layer.extent().intersect(orig_raster_layer.extent())
 
             # Skip any mask layers that don't overlap with the raster file
             if overlap.isEmpty():
                 continue
 
-            # Send some information to the user
-            feedback.pushInfo(str(mask_filename) +
-                              " intersects the raster at " + overlap.toString())
-
-            # Clip the raster layer by the mask layer
-            clipped_raster_filename = os.path.join(
-                dest_folder, mask_filename + ".tif"
-            )
+            # Clip the raster layer with the mask layer
+            clipped_raster_filename = os.path.join(dest_folder, mask_filename + ".tif")
             processing.run(
                 "gdal:cliprasterbymasklayer",
                 {
                     "INPUT": raster_path,
                     "MASK": mask_filename,
-                    "TARGET_EXTENT": f'{overlap.xMinimum()}, {overlap.xMaximum()}, {overlap.yMinimum()}, {overlap.yMaximum()}',
+                    "TARGET_EXTENT": f"{overlap.xMinimum()}, {overlap.xMaximum()}, {overlap.yMinimum()}, {overlap.yMaximum()}",
                     "MULTITHREADING": False,
                     "OUTPUT": clipped_raster_filename,
-                }
+                },
             )["OUTPUT"]
 
             # Load the clipped raster layer and get its height and width
@@ -327,70 +332,62 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
 
             larger_bed_axis = max(bed_length, bed_width)
             smaller_bed_axis = min(bed_length, bed_width)
-            larger_layer_axis = max(clipped_raster_layer.height(), clipped_raster_layer.width())
-            smaller_layer_axis = min(clipped_raster_layer.height(),
-                                     clipped_raster_layer.width())
-
-            # Send some information to the user
-            feedback.pushInfo(str(larger_bed_axis) + ", " + str(smaller_bed_axis) + ", "
-                              + str(larger_layer_axis) + ", " + str(smaller_layer_axis))
+            larger_layer_axis = max(
+                clipped_raster_layer.height(), clipped_raster_layer.width()
+            )
+            smaller_layer_axis = min(
+                clipped_raster_layer.height(), clipped_raster_layer.width()
+            )
 
             # Get the min scale factor needed to downscale the raster to fit in the print bed
             bed_scale_factor = min(
                 bed_scale_factor,
                 (larger_bed_axis / line_width) / larger_layer_axis,
-                (smaller_bed_axis / line_width) / smaller_layer_axis
+                (smaller_bed_axis / line_width) / smaller_layer_axis,
             )
 
             # Add the clipped raster to the list of rasters to process
             rasters_to_process.append(clipped_raster_layer)
 
         # Merge all the relevant raster files together
-        output = processing.run("gdal:merge", {
-            "INPUT": rasters_to_process,
-            "OUTPUT": "TEMPORARY_OUTPUT"
-        })["OUTPUT"]
+        output = processing.run(
+            "gdal:merge", {"INPUT": rasters_to_process, "OUTPUT": "TEMPORARY_OUTPUT"}
+        )["OUTPUT"]
         merged_raster = QgsRasterLayer(output)
 
         # Calculate the min scale factor needed to downscale the raster to fit the user's parameters
         larger_total_axis = max(total_length, total_width)
         smaller_total_axis = min(total_length, total_width)
-        larger_raster_axis = max(
-            merged_raster.height(), merged_raster.width())
-        smaller_raster_axis = min(
-            merged_raster.height(), merged_raster.width())
-
-        # Send some information to the user
-        feedback.pushInfo("Total vs Raster: " + str(larger_total_axis) + ", " + str(smaller_total_axis) + ", "
-                          + str(larger_raster_axis) + ", " + str(smaller_raster_axis))
+        larger_raster_axis = max(merged_raster.height(), merged_raster.width())
+        smaller_raster_axis = min(merged_raster.height(), merged_raster.width())
 
         total_raster_scale_factor = min(
-            (larger_total_axis / line_width) / larger_raster_axis, (smaller_total_axis / line_width) / smaller_raster_axis)
+            (larger_total_axis / line_width) / larger_raster_axis,
+            (smaller_total_axis / line_width) / smaller_raster_axis,
+        )
 
-        if total_raster_scale_factor > bed_scale_factor:
+        if total_raster_scale_factor <= bed_scale_factor:
+            scale_factor = total_raster_scale_factor
+
+        else:
             scale_factor = bed_scale_factor
 
             feedback.pushInfo(
-                "*** WARNING: The total height/width of the STL files will be smaller than expected so that all STLs can fit on the print bed")
-        else:
-            scale_factor = total_raster_scale_factor
+                "*** WARNING: The total height/width of the STL files will be smaller than expected so that all STLs can fit on the print bed"
+            )
+            feedback.pushInfo(
+                f"The new total height and width are {(merged_raster.height() * line_width) * scale_factor} mm and {(merged_raster.width() * line_width) * scale_factor} mm respectively.\n"
+            )
 
-        # Send some information to the user
-        feedback.pushInfo(
-            "The calculated scale factor is " + str(scale_factor))
-        feedback.pushInfo(
-            f"The total height and width are {(merged_raster.height() * line_width) * scale_factor} and {(merged_raster.width() * line_width) * scale_factor} respectively.")
+        # **************************************************************************************************
+        # 5) GENERATE AN STL FOR EACH CLIPPED RASTER LAYER
 
+        generated_STLs: list[str] = []
 
         # Generates an STL from each of the clipped raster layers
-        generated_STLs: list[str] = []
         for clipped_raster_layer in rasters_to_process:
             width = (clipped_raster_layer.height() * line_width) * scale_factor
             height = (clipped_raster_layer.width() * line_width) * scale_factor
-
-            # Send some information to the user
-            feedback.pushInfo(
-                f"Width and height: {width}, {height}")
 
             result = processing.run(
                 "stl_generator:stlfromraster",
@@ -402,17 +399,19 @@ class SplitThenGenerateSTLs(QgsProcessingAlgorithm):
                     "BED LENGTH": height,
                     "LINE WIDTH": line_width,
                     "OUTPUT": dest_folder,
-                }
+                },
             )
 
             if result["SUCCESS"]:
-                generated_STLs.append(result["OUTPUT"])
+                stl_filename = result["OUTPUT"]
+
+                generated_STLs.append(stl_filename)
 
                 # Send some information to the user
-                feedback.pushInfo(f"Made an stl for the raster file: {clipped_raster_layer.source()}\n")
-
-        # Send some information to the user
-        feedback.pushInfo("DONE")
+                feedback.pushInfo(f"Created a new STL: {stl_filename}")
+                feedback.pushInfo(
+                    f"Its height and width are {height} mm and {width} mm\n"
+                )
 
         # Return the results of the algorithm
         return {self.SUCCESS: True, self.OUTPUT: generated_STLs}
