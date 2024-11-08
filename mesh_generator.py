c90780410fc60b1c608889af4954762d6acbcf3a
Suheyb Aden <suheyb1@gmail.com>
2024-11-08 15:45:55 -0800

    Fixed a bug with calculating the vertical exaggeration

--- mesh_generator.py
+++ mesh_generator.py
@@ -60,13 +60,7 @@ class NoValidPixelsError (MeshGeneratorError):
 class MeshGenerator:
     def __init__(self):
         # Setup the logger
-        logger_filepath = '/home/suheyb/projects/africa_terrain/logging.log'#os.path.join(os.path.dirname(__file__), "logging.log")
-
-        # Check if the directory is writable
-        if os.access("/home/suheyb/projects/africa_terrain/", os.W_OK):
-            QgsMessageLog.logMessage(f"The directory is writeable", "MeshGenerator", level=Qgis.Warning)
-        else:
-            QgsMessageLog.logMessage(f"The directory is not writeable", "MeshGenerator", level=Qgis.Warning)
+        logger_filepath = os.path.join(os.path.dirname(__file__), "logging.log")
 
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(
@@ -74,20 +68,15 @@ class MeshGenerator:
             encoding='utf-8',
             filemode='a',
             level=logging.DEBUG,
-            format='%(asctime)s - %(name)-12s - %(levelname)-8s - %(message)s',
+            format='%(asctime)s - %(name)-12s - %(levelname)-8s : %(message)s',
             datefmt='%m/%d/%Y %I:%M:%S %p'
         )
 
-        try:
-            logging.info("Testing the root logger!\n")
-        except Exception as e:
-            QgsMessageLog.logMessage(f"Logging failed with error: {e}", "MeshGenerator", level=Qgis.Info)
-        
+        self.logger.info("Started a new logging session!")
+
         # Check if the log file was created
-        if os.path.exists(logger_filepath):
-            QgsMessageLog.logMessage(f"The log file '{logger_filepath}' was created successfully.", "MeshGenerator", level=Qgis.Info)
-        else:
-            QgsMessageLog.logMessage(f"The log file '{logger_filepath}' was not created.", "MeshGenerator", level=Qgis.Info)
+        if not os.path.exists(logger_filepath):
+            QgsMessageLog.logMessage(f"The log file '{logger_filepath}' could not be created!", "STL_Generator", level=Qgis.Warning)
 
         # Define initial parameter values
         self.verticalExaggeration = .1
@@ -121,6 +110,8 @@ class MeshGenerator:
         self.bedY = parameters["bedY"]
         self.lineWidth = parameters["lineWidth"]
 
+        self.name = os.path.basename(self.saveLocation)
+
     def generate_height_array(self, source_dem):
 
         # Opens the raster file being used
@@ -133,28 +124,28 @@ class MeshGenerator:
         # Check that the raster has a valid no data value
         self.noDataValue = band.GetNoDataValue()
         if (self.noDataValue is None):
-            self.noDataValue = -9999
+            self.noDataValue = -9999.0
+            self.logger.warning(f"The no data value is null so it is assumed to be -9999.0")
+        else:
             self.logger.info(f"The no data value is {self.noDataValue}")
 
-        self.logger.info(f"The no data value is {self.noDataValue}")
-
         # Gets the maximum resolution of the printer on each axis
-        larger_bed_axis = max(math.ceil(self.bedX/self.lineWidth), math.ceil(self.bedY/self.lineWidth))
-        smaller_bed_axis = min(math.ceil(self.bedX/self.lineWidth), math.ceil(self.bedY/self.lineWidth))
+        larger_bed_axis = max(math.ceil(self.bedX), math.ceil(self.bedY))
+        smaller_bed_axis = min(math.ceil(self.bedX), math.ceil(self.bedY))
 
-        self.logger.info(f"The bed size for {self.saveLocation} is {larger_bed_axis} by {smaller_bed_axis}\n")
+        self.logger.info(f"The bed size for {self.name} is {larger_bed_axis} by {smaller_bed_axis}")
 
         # Loads the x and y lengths of the raster
         larger_img_axis = max(dem.RasterXSize, dem.RasterYSize)
         smaller_img_axis = min(dem.RasterXSize, dem.RasterYSize)
 
-        self.logger.info(f"The image size for {source_dem} is {larger_img_axis} by {smaller_img_axis}\n")
+        self.logger.info(f"The image size is {larger_img_axis} by {smaller_img_axis}")
 
         # Gets the scaling factor needed to preserve the image ratio
         # while not going over the maximum resolutions of the printer
-        scalingFactor = min(1, larger_bed_axis / larger_img_axis, smaller_bed_axis / smaller_img_axis)
+        scalingFactor = min(1, larger_bed_axis / (self.lineWidth * larger_img_axis), smaller_bed_axis / (self.lineWidth * smaller_img_axis))
 
-        self.logger.info(f"The scale factor for {source_dem} is {scalingFactor}")
+        self.logger.info(f"The scale factor for {self.name} is {scalingFactor}")
 
         # Load the raster file as an array
         self.array = band.ReadAsArray(buf_xsize=math.ceil(dem.RasterXSize * scalingFactor),
@@ -163,6 +154,9 @@ class MeshGenerator:
                                       buf_type=gdal.GDT_Float32,
                                       resample_alg=gdal.GRIORA_NearestNeighbour)
 
+        self.logger.info(f"The target STL size is {self.bedX / self.lineWidth} by {self.bedY / self.lineWidth}")
+        self.logger.info(f"The acheived STL size is {self.array.shape[0]} by {self.array.shape[1]}")
+
         # *************************** GET VERTICAL EXAGGERATION OF IMAGE *************************** #
         # Load stats from the raster image
         minValue = band.GetMinimum()
@@ -170,15 +164,22 @@ class MeshGenerator:
         if not minValue or not maxValue:
             (minValue, maxValue) = band.ComputeRasterMinMax(True)
 
-        self.verticalExaggeration = self.printHeight / (maxValue - minValue)
-        self.bottomLevel = (
-            minValue * self.verticalExaggeration) - self.baseHeight
+        self.logger.info(f"The minimum and maximum values of the raster are {minValue} and {maxValue} respectively.")
 
+        # Calculate the vertical exaggeration
+        self.verticalExaggeration = self.printHeight / (self.lineWidth * (maxValue - minValue))
+        self.bottomLevel = (
+            minValue * self.verticalExaggeration) - (self.baseHeight / self.lineWidth)
+
+        self.logger.info(f"The vertical exaggeration is {self.verticalExaggeration}.")
+        self.logger.info(f"The new minimum and maximum values of the raster are {minValue * self.verticalExaggeration} and {maxValue * self.verticalExaggeration} respectively.")
+        self.logger.info(f"The bottom level of the model is {self.bottomLevel}.")
+
+        # Apply the vertical exaggeration
         self.array *= self.verticalExaggeration
         self.noDataValue *= self.verticalExaggeration
-        
-        self.logger.info(f"The minimum and maximum values of the raster are {minValue} and {maxValue} respectively.")
-        self.logger.info(f"")
+
+        self.logger.info(f"Applied the vertical exaggeration to the noDataValue. The new noDataValue is {self.noDataValue}")
 
     # Function for manually generating STL
     def manually_generate_stl(self):
@@ -191,8 +192,12 @@ class MeshGenerator:
         self.lib.generateSTL.restype = None
 
         try:
+            self.logger.info("Sending the image data and parameters to the meshgenerator library...")
             self.lib.generateSTL(self.array.astype(np.float32), self.array.shape[0], self.array.shape[1], self.noDataValue,
                                  self.lineWidth, self.bottomLevel, bytes(self.saveLocation, 'utf-8'))
 
         except Exception as e:
+            self.logger.critical("Library function call failed!")
             raise DLLFunctionFailedError("generateSTL")
+
+        self.logger.info("Library function call succeeded!\n")
